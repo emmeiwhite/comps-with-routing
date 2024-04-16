c371cbdb0ce90c97a67e45a48a3ec4a70dab0789
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-16 14:55:48 +0530

    Children prop used in Button Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import Button from "./Button";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Buy Now</Button>
+      </div>
+      <div>
+        <Button>Subscribe</Button>
+      </div>
+      <div>
+        <Button>Login</Button>
+      </div>
+      <div>
+        <Button>Register</Button>
+      </div>
+      <div>
+        <Button>Add To Cart</Button>
+      </div>
+    </div>
+  );
 }
