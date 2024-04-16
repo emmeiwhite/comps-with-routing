d7dde9d466780b56e0eb1b8ef117218b0bd5a903
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-04-16 16:45:59 +0530

    Made use of classnames library to add classes

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import classNames from "classnames";
 
 export default function Button({
   children,
@@ -10,11 +10,10 @@ export default function Button({
   success,
   danger,
 }) {
-  return (
-    <button className="border my-2 mx-3 p-2 bg-red-500 text-white">
-      {children}
-    </button>
-  );
+  const classes = classNames("border my-2 mx-3 px-3 py-1.5", {
+    "bg-blue-500 border-blue-500 text-white": primary,
+  });
+  return <button className={classes}>{children}</button>;
 }
 
 Button.propTypes = {
